d0c54f7ccfd911d7db5b99b5af8a9b94325f7595
Kumar Kunal <kumarkunal@apples-MacBook-Pro.local>
2019-01-26 20:23:38 +0530

    Lamdaexpression experiments

--- LambdaExpressions/src/com/lambda/ExecutionLambda.java
+++ LambdaExpressions/src/com/lambda/ExecutionLambda.java
@@ -1,6 +1,14 @@
 package com.lambda;
 
 public class ExecutionLambda {
+	
+	interface OperationInner{
+		public int perfomrInnerOperation(int x, int y);
+	}
+	
+	private int operate(int x, int y, OperationInner anInnerOp) {
+		return anInnerOp.perfomrInnerOperation(x, y);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -13,9 +21,35 @@ public class ExecutionLambda {
 		int returnedValue = fintObj.abstractFunction(5);
 		
 		System.out.println("Returned value is "+ returnedValue);
-
+		
+		InterfaceZeroArg zeroArgObj =  () -> {
+			System.out.println("DXrawing with zero inetrafce");
+		};
+		zeroArgObj.draw();
+		
+		Operation anAddOp1 = (int x,int y)->{
+			return x+y;
+		};
+		
+		Operation anMulOp = (int x, int y)->{
+			return x*y;
+		};
+		
+		System.out.println("Add lambda: " + anAddOp1.performOperation(4, 5));
+		System.out.println("Mul lambda: " + anMulOp.performOperation(4, 5));
+		
+		ExecutionLambda anExeObj = new ExecutionLambda();
+		
+		OperationInner anInnerSubOp = (int x,int y)->{
+			return x-y;
+		};
+		
+		OperationInner anInnerDivOp = (int x, int y)->{
+			return x/y;
+		};
+		
+		System.out.println("substraction operation by passing as parameter"+anExeObj.operate(6, 7, anInnerSubOp));
+		
 	}
-	
-	
 
 }
---
+++ LambdaExpressions/src/com/lambda/Operation.java
@@ -0,0 +1,6 @@
+package com.lambda;
+
+public interface Operation {
+	public int performOperation(int x, int y);
+
+}
